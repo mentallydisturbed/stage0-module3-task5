49ec93e0857a1c0a425b9cbac3e41f95ab847a8d
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 19:55:55 +0600

    fix2mod3task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,6 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        System.out.println(toBeDivided / 5);
+        System.out.println(toBeDivided / 5.);
     }
+//    public static void main(String[] args) {
+//        new NumberDivider().divide(19);
+//    }
 }
